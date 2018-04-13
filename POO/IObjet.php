3470972a1225a7ccbef4762425af007162e339d9
JeremLa <jeremy.lahore@gmail.com>
2018-04-13 19:13:15 +0200

    Update IObjet.php

--- POO/IObjet.php
+++ POO/IObjet.php
@@ -5,9 +5,9 @@ echo '<a href="poo.php">&larr; Retour</a><br><br>';
 echo '<i>';
 echo "Une interface permet de définir la signature d'une fonction.";
 echo '<br>';
-echo "Toutes les class qui implémantent cette interface, devront définir ces fonctions";
+echo "Toutes les classes qui implémantent cette interface, devront définir ces fonctions";
 echo '<br>';
-echo "Une signature, c'est l'entête d'une fonction sans son corp (<b><del>{}</del></b>)";
+echo "Une signature, c'est l'entête d'une fonction sans son corps (<b><del>{}</del></b>)";
 echo '<br>';
 echo "Vous trouverez un exemple d'interface dans le fichier IObjet.php";
 
@@ -15,7 +15,7 @@ interface IObjet {
     /**
      * On défini l'accès, le nom, les paramètres et le retour.
      * Il peut ne pas y avoir de paramètre et / ou de retour.
-     * Une interface ne peu définir que des function public
+     * Une interface ne peut définir que des fonctions publiques
      * Public étant implicite, il n'est pas obligatoire de
      * le préciser
      */
